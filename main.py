e09f1297ea2daadea5bce251341e92479e2e4391
Wes Henderson <whendersonii@gmail.com>
2023-06-02 17:52:42 -0500

    Playing with roles.

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 from os import path
 import configparser
 import openai
+import yaml
+
+RESUME_FILE = "../weshenderson.github.io/configs/resume.yaml"
+
+with open(RESUME_FILE, encoding="UTF-8") as file:
+    content = yaml.safe_load(file)
 
 AUTH_FILE = f"{path.expanduser('~')}/.keys"
-CONTENT = ""
+CONTENT = f"Please help me improve this resume summary: {content['basics']['summary']}"
 
 config = configparser.RawConfigParser()
 config.read(AUTH_FILE)
@@ -15,6 +21,12 @@ openai.api_key = api_key
 
 #print(openai.Model.list())
 
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": CONTENT}])
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": CONTENT},
+        {"role": "system", "content": "You are and expert on resumes and IT recruiting."}
+    ]
+)
 
-print(chat_completion)
+print(response)
